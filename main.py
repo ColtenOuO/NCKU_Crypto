edc38e1c5a60da4c6f97fc731be7a59fee263d43
ColtenOuO <jun930436@gmail.com>
2023-10-16 01:03:06 +0800

    ok

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from Crypto.Cipher import AES
 import base64
 import itertools
 
-# 加密参数
 key = "|q▆~k=▆&?I$Fx▆N▆"
 mode = AES.MODE_ECB
 padding = '\x00'
 
-# 加密后的Base64编码密文
 cipher_text = base64.b64decode("FZp57a6p84EUNC7I/ENj4RhPZtryOJr4che9JbA8ng1eI8ZMTlsl8kzicBDqkOqkFj3lwC69KR2MeA8lscVlig==")
 
-# 字符集合，包括可能的字符
 charset = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
 
-# 所有可能的字符替代组合
 combinations = itertools.product(charset, repeat=key.count('▆'))
 
 for replacement_chars in combinations:
@@ -26,5 +22,4 @@ for replacement_chars in combinations:
         plaintext = decryptor.decrypt(cipher_text).rstrip(padding.encode('utf-8'))
         print(f"GET：{plaintext.decode('utf-8')}")
     except:
-        # 解密失败，继续下一个字符的尝试
         continue
