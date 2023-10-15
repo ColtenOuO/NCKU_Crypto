14c7381b9f70c2bd6e36ed433fb6800281821818
ColtenOuO <jun930436@gmail.com>
2023-10-16 01:35:22 +0800

    test

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from Crypto.Cipher import AES
 import base64
 import itertools
 
-key = "|q▆~k=▆&?I$Fx▆N▆"
+key = "|qL~k=S&?I$Fx+N("
 mode = AES.MODE_ECB
 padding = '\x00'
 
 cipher_text = base64.b64decode("FZp57a6p84EUNC7I/ENj4RhPZtryOJr4che9JbA8ng1eI8ZMTlsl8kzicBDqkOqkFj3lwC69KR2MeA8lscVlig==")
 
-charset = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
-
+charset = 'LS!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+#|qL~k=S&?I$Fx+N(
 combinations = itertools.product(charset, repeat=key.count('▆'))
 
 for replacement_chars in combinations:
